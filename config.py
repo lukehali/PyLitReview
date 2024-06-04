5475c8d859c9c4287b246280104c32f28e811b50
lukehali <haliburton.luke@gmail.com>
2024-06-04 17:56:26 +0200

    working on it

---
+++ config.py
@@ -0,0 +1,10 @@
+UNI_MAIL = "luke.haliburton@ifi.lmu.de" 
+UNI_PWD = "40Schmee" #LRZ-PW
+UNI_USER = "Luke.Haliburton" #LRZ-Kennung
+
+# downloadfolder should look like: C:\\Users\\User\\Path\\To\\PyLitReview\\\\Library
+# e.g. C:\\Users\\User\\Documents\\GitHub\\PyLitReview\\acm
+downloadfolder_acm = "C:\\Users\\halib\\Coding Projects\\PyLitReview\\PyLitReview\\acm"
+downloadfolder_ieee = ""
+downloadfolder_sd = ""
+downloadfolder_default = ""
